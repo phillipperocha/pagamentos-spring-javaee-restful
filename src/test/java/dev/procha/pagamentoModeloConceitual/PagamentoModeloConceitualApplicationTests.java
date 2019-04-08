7340ccbc62a3dc346dfcbe544c1348389a1ae0f8
Phillippe Rocha <phillipperocha@gmail.com>
2019-04-08 00:16:10 -0300

    Criação da aplicação

---
+++ src/test/java/dev/procha/pagamentoModeloConceitual/PagamentoModeloConceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.procha.pagamentoModeloConceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PagamentoModeloConceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
